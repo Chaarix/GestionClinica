16932581688253ce21e45dda4e53b676df01d470
xluuleex <lucia.mllmn@gmail.com>
2026-09-09 19:34:52 -0300

    preview interfaz

---
+++ src/main/java/com/losalerces/sistematurnos/controladores/controladorDoctores.java
@@ -0,0 +1,11 @@
+package com.losalerces.sistematurnos.controladores;
+
+import javafx.event.ActionEvent;
+
+public class controladorDoctores {
+    public void agregarDoctor(ActionEvent actionEvent) {
+    }
+
+    public void buscarDoctor(ActionEvent actionEvent) {
+    }
+}
