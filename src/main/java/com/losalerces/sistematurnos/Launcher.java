cfaed119f3e761b3a630471705693327ccb3d892
Chaarix <1eliseotorres2007@gmail.com>
2026-09-10 18:16:54 -0300

    DAO de Turno, Paciente DoctorObraSocial

--- src/main/java/com/losalerces/sistematurnos/Launcher.java
+++ src/main/java/com/losalerces/sistematurnos/Launcher.java
@@ -2,7 +2,7 @@ package com.losalerces.sistematurnos;
 
 import com.losalerces.sistematurnos.Clases.ClaseObraSocial;
 import com.losalerces.sistematurnos.DAO.ObraSocialDAO;
-import javafx.application.Application;
+
 
 import java.util.List;
 import java.util.Scanner;
