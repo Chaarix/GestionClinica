d66bfdfd0e1c9ffdb95478976ddae42d71ff2a69
honored-star <odiaz7186@gmail.com>
2026-09-15 19:07:46 -0300

    se agrego Lombok a todas las clases

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,6 +10,8 @@ module com.losalerces.sistematurnos {
     requires com.almasb.fxgl.all;
     requires java.desktop;
     requires java.sql;
+    requires jdk.jfr;
+    requires static lombok;
 
     opens com.losalerces.sistematurnos to javafx.fxml;
     opens com.losalerces.sistematurnos.Controladores to javafx.fxml;
