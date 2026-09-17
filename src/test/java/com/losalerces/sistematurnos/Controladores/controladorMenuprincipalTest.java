9f465dc6ff65269750b3cb7dbcec4b7868857804
xluuleex <lucia.mllmn@gmail.com>
2026-09-17 18:43:43 -0300

    modifique la bd

---
+++ src/test/java/com/losalerces/sistematurnos/Controladores/controladorMenuprincipalTest.java
@@ -0,0 +1,12 @@
+package com.losalerces.sistematurnos.Controladores;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class controladorMenuprincipalTest {
+
+    @Test
+    void initialize() {
+    }
+}
