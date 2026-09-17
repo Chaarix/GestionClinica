6035c2f67178831bde687b3ca746cee33aae1bf9
xluuleex <lucia.mllmn@gmail.com>
2026-09-17 18:43:43 -0300

    modifique la bd

--- src/test/java/com/losalerces/sistematurnos/DAO/PacienteDAOTest.java
+++ src/test/java/com/losalerces/sistematurnos/DAO/PacienteDAOTest.java
@@ -33,4 +33,40 @@ class PacienteDAOTest {
     @Test
     void listarPacientesPamiConTurnosCumplidos() {
     }
+
+    @Test
+    void testGuardar() {
+    }
+
+    @Test
+    void testAgregar() {
+    }
+
+    @Test
+    void testListar() {
+    }
+
+    @Test
+    void testBuscarPorId() {
+    }
+
+    @Test
+    void testActualizar() {
+    }
+
+    @Test
+    void testEliminar() {
+    }
+
+    @Test
+    void testListarPacientesPamiConTurnosCumplidos() {
+    }
+
+    @Test
+    void existeDni() {
+    }
+
+    @Test
+    void existeDniEnOtroPaciente() {
+    }
 }
