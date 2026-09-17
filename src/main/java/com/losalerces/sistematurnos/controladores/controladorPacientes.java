e0904a5a5b3826af689447a810c15434b577d548
Chaarix <1eliseotorres2007@gmail.com>
2026-09-17 18:46:44 -0300

    asd

--- src/main/java/com/losalerces/sistematurnos/controladores/controladorPacientes.java
+++ src/main/java/com/losalerces/sistematurnos/controladores/controladorPacientes.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 public class controladorPacientes {
     public void agregarPaciente(ActionEvent actionEvent) {
     }
+    //55
 
     public void buscarPaciente(ActionEvent actionEvent) {
     }
