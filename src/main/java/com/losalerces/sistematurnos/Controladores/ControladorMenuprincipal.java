38c08ca4d9cf918db5de0790e7011f9726d6587e
Chaarix <1eliseotorres2007@gmail.com>
2026-09-17 21:51:46 -0300

    Solucionar conflicto de mayusculas en el indice

--- src/main/java/com/losalerces/sistematurnos/Controladores/ControladorMenuprincipal.java
+++ src/main/java/com/losalerces/sistematurnos/Controladores/ControladorMenuprincipal.java
@@ -1,8 +1,7 @@
-package com.losalerces.sistematurnos.controladores;
+package com.losalerces.sistematurnos.Controladores;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -10,7 +9,9 @@ import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.util.Duration;
 
 import java.io.IOException;
@@ -21,7 +22,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
-public class controladorMenuprincipal implements Initializable {
+public class ControladorMenuprincipal implements Initializable {
 
     @FXML
     private StackPane contenedorPrincipal;
@@ -61,7 +62,10 @@ public class controladorMenuprincipal implements Initializable {
         cargarVista("doctores.fxml");
     }
 
-
+    @FXML
+    private void mostrarPacientes(MouseEvent actionEvent) {
+        cargarVista("pacientes.fxml");
+    }
 
     private void cargarVista(String archivo) {
         try {
@@ -90,13 +94,27 @@ public class controladorMenuprincipal implements Initializable {
 
         java.io.InputStream stream = getClass().getResourceAsStream("/imagen/logo_clinica.png");
 
-        if (stream != null) {
+        if (stream != null && iv != null) {
             iv.setImage(new Image(stream));
         } else {
             System.err.println("Error crítico: No se encontró el archivo");
         }
     }
+    //AAAAAAAAAAAAAAAAAAAA
 
-    public void mostrarPacientes(ActionEvent actionEvent) {
+
+
+    @FXML
+    private void mostrarReportePami() {
+        try {
+            // Forma correcta de inicializar el FXMLLoader con la ruta de la vista
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/losalerces/sistematurnos/reportePami.fxml"));
+            VBox vistaReporte = loader.load();
+
+            contenedorPrincipal.getChildren().setAll(vistaReporte);
+        } catch (IOException e) {
+            System.out.println("Error al cargar la vista de Reporte PAMI: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
